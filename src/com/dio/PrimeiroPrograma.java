2198271bb82ddd0f83860febcd67c054f18aed8e
gabriel0929 <gabriel.ggoncar@gmail.com>
2022-03-18 16:39:07 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+	
+	Gato gato = new Gato();
+	System.out.println(gato);
+	
+	
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+	}
+*/
+}
+}
